cce82aaf365c6b6b6ee19f79de79a7ebea6a7c7f
Saroj Maharjan <sarojmhrzn@gmail.com>
2017-08-04 18:03:03 +0545

    adds question , category

--- frontend/src/app/dashboard/dashboard.service.ts
+++ frontend/src/app/dashboard/dashboard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 
+import { Question } from './question/quest';
 import { Category } from './categories/category';
 
 @Injectable()
@@ -18,6 +19,37 @@ export class DashboardService {
         category.categoryName = categoryObj.categoryName;
 
         return category;
+      })
+      .catch(this.handleError);
+  }
+
+  listCategory(): Observable<Category> {
+    return this.http.get('http://localhost:3000/categories')
+      .map(response => {
+        let categories: Category[] = [];
+        response.json().forEach((categoryObj) => {
+          let category = new Category();
+          category.id = categoryObj.id;
+          category.categoryName = categoryObj.categoryName;
+
+          categories.push(category)
+        });
+
+        return categories;
+      })
+      .catch(this.handleError);
+  }
+
+  createQuestion(quesObj): Observable<Question> {
+    return this.http.post('http://localhost:3000/question', quesObj)
+      .map(response => {
+        const quesObj = response.json();
+        let questions = new Question();
+        questions.question = quesObj.question;
+        questions.incorrectAnswer = quesObj.incorrectAnswer;
+        questions.correctAnswer = quesObj.correctAnswer;
+
+        return questions;
       })
       .catch(this.handleError);
   }
--- frontend/src/app/dashboard/question/question.component.ts
+++ frontend/src/app/dashboard/question/question.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DashboardService } from '../dashboard.service';
+import { Category } from '../categories/category';
+import { Question } from './quest';
+
 @Component({
   selector: 'app-add-question-page',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
+  providers: [DashboardService]
 })
 export class QuestionComponent implements OnInit {
+  categories: Category;
+  quest = new Question();
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) {
+  }
 
   ngOnInit() {
+    this.listCategory();
+  }
+
+  listCategory(){
+    this.dashboardService.listCategory()
+      .subscribe((categories) => {
+        this.categories = categories;
+      })
+  }
+
+  createQuestion() {
+    this.dashboardService.createQuestion(this.quest)
+      .subscribe((questions) => {
+        this.quest = questions;
+      })
+  }
+
+  refresh():void {
+    window.location.reload();
   }
 
 }
